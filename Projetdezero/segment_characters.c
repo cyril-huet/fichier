03e7891ae1173332ca0078ff11cc00d01af7aca2
HUET Cyril <cyril.huet@epita.fr>
2024-10-28 19:53:31 +0100

    commit 1

--- Projetdezero/segment_characters.c
+++ Projetdezero/segment_characters.c
@@ -1,18 +1,16 @@
 #include <SDL2/SDL.h>
 #include <stdio.h>
+#include <string.h>
 
-// Dimensions maximales et minimales d'un caractère (à ajuster selon l'image)
 #define MAX_CHAR_WIDTH 50
 #define MAX_CHAR_HEIGHT 50
 #define MIN_CHAR_WIDTH 5
 #define MIN_CHAR_HEIGHT 5
 
-// Marquer les pixels déjà traités
-int processed_pixels[1000][1000];  // Ajuster en fonction de la taille de l'image
+int processed_pixels[1000][1000];
 
-// Vérifie si un pixel est noir et n'a pas été déjà traité
 int is_black_pixel(SDL_Surface *surface, int x, int y) {
-    if (processed_pixels[y][x]) return 0;  // Ignore les pixels déjà traités
+    if (processed_pixels[y][x]) return 0;
 
     Uint32 pixel = ((Uint32*)surface->pixels)[y * surface->w + x];
     Uint8 r, g, b;
@@ -21,7 +19,6 @@ int is_black_pixel(SDL_Surface *surface, int x, int y) {
     return (r == 0 && g == 0 && b == 0);
 }
 
-// Trouve les limites d'un caractère
 void find_character_bounds(SDL_Surface *surface, int startX, int startY, int *left, int *right, int *top, int *bottom) {
     *left = *right = startX;
     *top = *bottom = startY;
@@ -33,19 +30,16 @@ void find_character_bounds(SDL_Surface *surface, int startX, int startY, int *le
                 if (x > *right) *right = x;
                 if (y < *top) *top = y;
                 if (y > *bottom) *bottom = y;
-
-                processed_pixels[y][x] = 1;  // Marque le pixel comme traité
+                processed_pixels[y][x] = 1;
             }
         }
     }
 }
 
-// Enregistre l'image d'un caractère si elle dépasse une taille minimale
 void save_character(SDL_Surface *surface, int left, int top, int right, int bottom, int index) {
     int charWidth = right - left + 1;
     int charHeight = bottom - top + 1;
 
-    // Ignore les petits artefacts
     if (charWidth < MIN_CHAR_WIDTH || charHeight < MIN_CHAR_HEIGHT) return;
 
     SDL_Surface *charSurface = SDL_CreateRGBSurface(0, charWidth, charHeight, 32, 0x00FF0000, 0x0000FF00, 0x000000FF, 0xFF000000);
@@ -64,24 +58,40 @@ void save_character(SDL_Surface *surface, int left, int top, int right, int bott
     SDL_FreeSurface(charSurface);
 }
 
+// Vérifie si une ligne est vide
+int is_empty_line(SDL_Surface *surface, int y) {
+    for (int x = 0; x < surface->w; x++) {
+        if (is_black_pixel(surface, x, y)) return 0;
+    }
+    return 1;
+}
+
 void segment_characters(SDL_Surface *surface) {
     int index = 0;
-
-    // Initialiser le tableau des pixels traités
     memset(processed_pixels, 0, sizeof(processed_pixels));
 
-    for (int y = 0; y < surface->h; y++) {
+    int y = 0;
+    while (y < surface->h) {
+        if (is_empty_line(surface, y)) {
+            y++;  // Passe à la ligne suivante si elle est vide
+            continue;
+        }
+
         for (int x = 0; x < surface->w; x++) {
             if (is_black_pixel(surface, x, y)) {
                 int left, right, top, bottom;
                 find_character_bounds(surface, x, y, &left, &right, &top, &bottom);
 
-                // Sauvegarde uniquement si les dimensions sont valides
                 save_character(surface, left, top, right, bottom, index);
                 index++;
 
-                x = right;  // Passer au pixel après le caractère
+                x = right;  // Passe au pixel suivant après le caractère
             }
+        }
+
+        // Passe à la prochaine ligne vide après avoir traité une ligne pleine
+        while (y < surface->h && !is_empty_line(surface, y)) {
+            y++;
         }
     }
 }
