d16fcdae3e3c81774003846bbed530e1a5cc0bbb
HUET Cyril <cyril.huet@epita.fr>
2024-10-29 17:33:27 +0100

    commit 1

--- OOOOOOO/test.c
+++ OOOOOOO/test.c
@@ -3,42 +3,64 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define TOLERANCE 10 // tolérance pour détecter les lignes de la grille
+#define THRESHOLD 128 // Seuil pour la binarisation
+#define MIN_CHARACTER_SIZE 20 // Taille minimale d'un caractère (en pixels)
 
-int *detect_lines(SDL_Surface *image, int *num_lines, int horizontal) {
-    int max_dim = horizontal ? image->h : image->w;
-    int *lines = (int *)malloc(max_dim * sizeof(int));
-    *num_lines = 0;
-
-    for (int i = 0; i < max_dim; i++) {
-        int is_line = 1;
-        for (int j = 0; j < (horizontal ? image->w : image->h); j++) {
-            Uint32 pixel = *((Uint32 *)image->pixels + (horizontal ? i * image->w + j : j * image->w + i));
+void binarize(SDL_Surface *image) {
+    for (int y = 0; y < image->h; y++) {
+        for (int x = 0; x < image->w; x++) {
+            Uint32 pixel = ((Uint32 *)image->pixels)[y * image->w + x];
             Uint8 r, g, b;
             SDL_GetRGB(pixel, image->format, &r, &g, &b);
+            Uint8 gray = (Uint8)(0.3 * r + 0.59 * g + 0.11 * b);
 
-            if (r > 200 && g > 200 && b > 200) { // Considérons les pixels clairs comme des espaces
-                is_line = 0;
-                break;
+            if (gray < THRESHOLD) {
+                ((Uint32 *)image->pixels)[y * image->w + x] = SDL_MapRGB(image->format, 0, 0, 0);
+            } else {
+                ((Uint32 *)image->pixels)[y * image->w + x] = SDL_MapRGB(image->format, 255, 255, 255);
             }
         }
-        if (is_line && (*num_lines == 0 || i - lines[*num_lines - 1] > TOLERANCE)) {
-            lines[(*num_lines)++] = i;
-        }
     }
-    return lines;
 }
 
-void save_letter(SDL_Surface *grid, int x, int y, int width, int height, int letter_index) {
-    SDL_Rect cell_rect = {x, y, width, height};
+void save_letter(SDL_Surface *image, int x, int y, int width, int height, int letter_index) {
+    SDL_Rect rect = {x, y, width, height};
     SDL_Surface *letter_surface = SDL_CreateRGBSurface(0, width, height, 32, 0x00FF0000, 0x0000FF00, 0x000000FF, 0xFF000000);
-    SDL_BlitSurface(grid, &cell_rect, letter_surface, NULL);
+    SDL_BlitSurface(image, &rect, letter_surface, NULL);
 
     char filename[50];
     snprintf(filename, sizeof(filename), "letters/letter_%d.png", letter_index);
     IMG_SavePNG(letter_surface, filename);
 
     SDL_FreeSurface(letter_surface);
+}
+
+void detect_characters(SDL_Surface *image) {
+    int letter_index = 0;
+
+    for (int y = 0; y < image->h; y += MIN_CHARACTER_SIZE) {
+        for (int x = 0; x < image->w; x += MIN_CHARACTER_SIZE) {
+            int black_pixels = 0;
+
+            // Compter les pixels noirs dans ce bloc
+            for (int j = 0; j < MIN_CHARACTER_SIZE && y + j < image->h; j++) {
+                for (int i = 0; i < MIN_CHARACTER_SIZE && x + i < image->w; i++) {
+                    Uint32 pixel = ((Uint32 *)image->pixels)[(y + j) * image->w + (x + i)];
+                    Uint8 r, g, b;
+                    SDL_GetRGB(pixel, image->format, &r, &g, &b);
+
+                    if (r == 0 && g == 0 && b == 0) { // Si le pixel est noir
+                        black_pixels++;
+                    }
+                }
+            }
+
+            // Si le bloc contient suffisamment de pixels noirs, on le considère comme une lettre
+            if (black_pixels > (MIN_CHARACTER_SIZE * MIN_CHARACTER_SIZE) / 4) {
+                save_letter(image, x, y, MIN_CHARACTER_SIZE, MIN_CHARACTER_SIZE, letter_index++);
+            }
+        }
+    }
 }
 
 int main(int argc, char *argv[]) {
@@ -61,36 +83,12 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    int num_horizontal_lines, num_vertical_lines;
-    int *horizontal_lines = detect_lines(grid_image, &num_horizontal_lines, 1);
-    int *vertical_lines = detect_lines(grid_image, &num_vertical_lines, 0);
+    // Étape 1 : Binarisation de l'image
+    binarize(grid_image);
 
-    if (num_horizontal_lines < 2 || num_vertical_lines < 2) {
-        fprintf(stderr, "Erreur : Impossible de détecter la grille.\n");
-        free(horizontal_lines);
-        free(vertical_lines);
-        SDL_FreeSurface(grid_image);
-        IMG_Quit();
-        SDL_Quit();
-        return 1;
-    }
+    // Étape 2 : Détection et sauvegarde des caractères
+    detect_characters(grid_image);
 
-    // Découpage et sauvegarde des lettres
-    int letter_index = 0;
-    for (int row = 0; row < num_horizontal_lines - 1; row++) {
-        for (int col = 0; col < num_vertical_lines - 1; col++) {
-            int x = vertical_lines[col];
-            int y = horizontal_lines[row];
-            int width = vertical_lines[col + 1] - x;
-            int height = horizontal_lines[row + 1] - y;
-
-            save_letter(grid_image, x, y, width, height, letter_index++);
-        }
-    }
-
-    // Libération de la mémoire
-    free(horizontal_lines);
-    free(vertical_lines);
     SDL_FreeSurface(grid_image);
     IMG_Quit();
     SDL_Quit();
