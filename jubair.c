61e1af4d131a0dd73fe7fcd3e6cd70481f933424
HUET Cyril <cyril.huet@epita.fr>
2024-10-31 23:21:40 +0100

    commit 1

--- jubair.c
+++ jubair.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
-#include <SDL.h>
-#include <SDL_image.h>
+#include <SDL2/SDL.h>
 
 #define THRESHOLD 128  // Seuil pour binarisation
 
@@ -31,38 +30,8 @@ void free_image(Image img) {
     free(img.data);
 }
 
-// Fonction pour lire une image en niveaux de gris
-Image load_png_image(const char *filename) {
-    SDL_Surface *surface = IMG_Load(filename);
-    if (!surface) {
-        fprintf(stderr, "Erreur d'ouverture du fichier %s: %s\n", filename, IMG_GetError());
-        exit(1);
-    }
-
-    if (surface->format->BytesPerPixel != 4) {
-        fprintf(stderr, "L'image doit être de format 32 bits.\n");
-        SDL_FreeSurface(surface);
-        exit(1);
-    }
-
-    Image img = create_image(surface->w, surface->h);
-    for (int y = 0; y < surface->h; y++) {
-        for (int x = 0; x < surface->w; x++) {
-            Uint32 pixel = *((Uint32 *)surface->pixels + y * surface->w + x);
-            Uint8 r, g, b, a;
-            SDL_GetRGBA(pixel, surface->format, &r, &g, &b, &a);
-            img.data[y * img.width + x] = (r + g + b) / 3;  // Convertir en niveaux de gris
-        }
-    }
-
-    SDL_FreeSurface(surface);
-    return img;
-}
-
-// Fonction pour sauvegarder une image en BMP (remplacez par PNG si nécessaire)
-// Fonction pour sauvegarder une image en BMP
+// Fonction pour sauvegarder une image en niveaux de gris en utilisant SDL
 void save_image(const char *filename, Image img) {
-    // Créer une surface SDL de la bonne taille
     SDL_Surface *surface = SDL_CreateRGBSurface(0, img.width, img.height, 8, 0, 0, 0, 0);
     if (!surface) {
         fprintf(stderr, "Erreur de création de la surface SDL: %s\n", SDL_GetError());
@@ -72,14 +41,14 @@ void save_image(const char *filename, Image img) {
     // Remplir la surface avec les données de l'image
     for (int y = 0; y < img.height; y++) {
         for (int x = 0; x < img.width; x++) {
-            Uint8 color = img.data[y * img.width + x];
-            ((Uint8 *)surface->pixels)[y * surface->pitch + x] = color;  // Assurer l'accès à l'index
+            Uint8 gray = img.data[y * img.width + x]; // Obtenir la valeur en niveaux de gris
+            surface->pixels[y * surface->pitch + x] = SDL_MapRGB(surface->format, gray, gray, gray); // Remplir le pixel
         }
     }
 
-    // Enregistrer l'image en tant que fichier BMP
-    if (SDL_SaveBMP(surface, filename) != 0) {
-        fprintf(stderr, "Erreur de sauvegarde de l'image %s: %s\n", filename, SDL_GetError());
+    // Sauvegarder l'image (en utilisant SDL_image)
+    if (IMG_SavePNG(surface, filename) != 0) {
+        fprintf(stderr, "Erreur lors de la sauvegarde de l'image: %s\n", IMG_GetError());
     }
 
     SDL_FreeSurface(surface);
@@ -162,7 +131,7 @@ void detect_and_extract_letters(Image *src) {
                     }
 
                     char filename[50];
-                    snprintf(filename, sizeof(filename), "letter_%d.bmp", label);  // Changer l'extension à BMP
+                    snprintf(filename, sizeof(filename), "letter_%d.png", label);
                     save_image(filename, letter);
                     free_image(letter);
                     label++;
@@ -178,22 +147,8 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    // Initialiser SDL
-    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-        fprintf(stderr, "Erreur d'initialisation SDL: %s\n", SDL_GetError());
-        return 1;
-    }
-
-    // Initialiser SDL_image
-    if (IMG_Init(IMG_INIT_PNG) == 0) {
-        fprintf(stderr, "Erreur d'initialisation SDL_image: %s\n", IMG_GetError());
-        SDL_Quit();
-        return 1;
-    }
-
     // Charger l'image source
-    Image src = load_png_image(argv[1]);
-    
+    Image src = load_png_image(argv[1]); // Vous pouvez adapter cette fonction pour utiliser SDL ou une autre méthode
     // Binariser l'image
     binarize_image(&src);
     
@@ -202,10 +157,6 @@ int main(int argc, char *argv[]) {
     
     // Libérer la mémoire de l'image source
     free_image(src);
-
-    // Nettoyer SDL_image et SDL
-    IMG_Quit();
-    SDL_Quit();
     
     return 0;
 }
