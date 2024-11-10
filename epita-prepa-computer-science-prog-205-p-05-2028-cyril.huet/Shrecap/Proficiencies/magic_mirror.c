a3294ad67cd44dc6fdac7747fc86b331271b9360
HUET Cyril <cyril.huet@epita.fr>
2024-11-10 16:24:42 +0100

    commit

---
+++ epita-prepa-computer-science-prog-205-p-05-2028-cyril.huet/Shrecap/Proficiencies/magic_mirror.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[]) {
+    // TODO - complete exercise 5: Fiona's Magic Mirror Matrix
+    return 0;
+}
+
