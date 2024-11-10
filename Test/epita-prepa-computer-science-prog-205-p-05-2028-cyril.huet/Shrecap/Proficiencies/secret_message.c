a9a5b6f138f0265298e7007994f110b56b85e135
HUET Cyril <cyril.huet@epita.fr>
2024-11-10 16:54:33 +0100

    commit

---
+++ Test/epita-prepa-computer-science-prog-205-p-05-2028-cyril.huet/Shrecap/Proficiencies/secret_message.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+
+int main(int argc, char *argv[]) {
+    // TODO - complete exercise 6: Puss in Boots’ Secret Message
+    return 0;
+}
+
